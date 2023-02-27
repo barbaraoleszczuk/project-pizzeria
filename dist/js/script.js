43d9a701f1143c73c7d401f40f411f8b78f268c3
Barbara Oleszczuk <basiaoleee@o2.pl>
2023-02-27 19:07:02 +0100

    Add initAccordion

--- dist/js/script.js
+++ dist/js/script.js
@@ -75,24 +75,27 @@
     
 
     initAccordion(){
-        const thisProduct = this;
+      const thisProduct = this;
     
-        /* find the clickable trigger (the element that should react to clicking) */
-        const clickableTrigger = ???;
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
     
-        /* START: add event listener to clickable trigger on event click */
-        clickableTrigger.addEventListener('click', function(event) {
-          /* prevent default action for event */
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = /*thisProduct.element.*/document.querySelector(select.all.menuProductsActive);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct !== null && activeProduct != thisProduct.element){
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
     
-          /* find active product (product that has active class) */
-    
-          /* if there is active product and it's not thisProduct.element, remove class active from it */
-    
-          /* toggle active class on thisProduct.element */
-        });
-    
-      }
     }
+  }
   
   const app = {
     initData: function(){
