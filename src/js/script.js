df2214f71a9508cb26d459315dd4cf3c5720acab
Barbara Oleszczuk <basiaoleee@o2.pl>
2023-03-11 18:49:57 +0100

    Add product to cart

--- src/js/script.js
+++ src/js/script.js
@@ -126,6 +126,7 @@
       // console.log('priceElem',thisProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
       thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
+      
     }
     initAccordion(){
       const thisProduct = this;
@@ -165,6 +166,7 @@
       thisProduct.cartButton.addEventListener('click', function(event){
         event.preventDefault();
         thisProduct.processOrder();
+        thisProduct.addToCart();
       });
     }
     processOrder(){
@@ -218,6 +220,8 @@
         }
         
       }
+      thisProduct.priceSingle = price; //aktualna cena jednostkowa- po zmianie opcji
+
       // multiply price by amount
       price *= thisProduct.amountWidget.value;
 
@@ -232,7 +236,66 @@
         thisProduct.processOrder();
       });
     }
-  } 
+    addToCart(){
+      const thisProduct = this;
+
+      app.cart.add(thisProduct.prepareCartProduct());
+    }
+    prepareCartProduct(){
+      const thisProduct = this;
+      
+      const productSummary = {
+        id: thisProduct.id,
+        name: thisProduct.data.name,
+        amount: thisProduct.amountWidget.value,
+        priceSingle: thisProduct.priceSingle,
+        price: thisProduct.priceSingle * thisProduct.amountWidget.value,
+        params: thisProduct.prepareCartProductParams(),
+      };
+  
+      return productSummary ;
+    }
+    prepareCartProductParams(){
+
+      const thisProduct = this;
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      // console.log('formData', formData);
+
+      const params = {};
+          
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+      // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+
+        const param = thisProduct.data.params[paramId];
+        
+        // create category param in params const eg. params = { ingredients: { name: 'Ingredients', options: {}}}
+        params[paramId] = {
+          label: param.label,
+          options: {},
+        };
+      
+        // for every option in this category
+        for(let optionId in param.options) {
+
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+
+          if(optionSelected){
+            // option is selected!
+            params[paramId].options[optionId] = option.label;
+          } 
+          
+        }
+          
+      }
+      return params;
+    
+    }
+  }
   class AmountWidget{ 
     constructor(element){
       const thisWidget = this;
@@ -311,13 +374,29 @@
 
       thisCart.dom.wrapper=element;
       thisCart.dom.toggleTrigger=thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+      thisCart.dom.productList=thisCart.dom.wrapper.querySelector(select.cart.productList);
     }
+  
     initActions (){
       const thisCart = this;
       thisCart.dom.toggleTrigger.addEventListener('click',function(event){
         event.preventDefault();
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
       });
+    }
+    add(menuProduct){
+      // const thisCart = this;
+      const thisCart = this; 
+      /*generate HTML based on template*/
+      const generatedHTML = templates.cartProduct(menuProduct);
+      // console.log(generatedHTML);
+      /*create element using utils.createElementFromHTML*/
+      const generatedDOM= utils.createDOMFromHTML(generatedHTML);
+      
+      /*add element to menu*/
+      thisCart.dom.productList.appendChild(generatedDOM);
+
+      console.log('adding product', menuProduct);
     }
   }
   const app = {
