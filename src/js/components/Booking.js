20d93bbecc0170732a073384e33abeacb203c81c
Barbara Oleszczuk <basiaoleee@o2.pl>
2023-04-16 11:33:20 +0200

    Add minDate

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -108,13 +108,15 @@ class Booking{
     for(let item of eventsCurrent){
       thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
     }
+    const minDate = thisBooking.datePicker.minDate;
+    const maxDate = thisBooking.datePicker.maxDate;
     for(let item of eventsRepeat){
       if(item.repeat == 'daily'){
         for(let loopDate = minDate; loopDate <= maxDate; loopDate = utils.addDays(loopDate, 1)){
           thisBooking.makeBooked(utils.dateToStr(loopDate), item.hour, item.duration, item.table);
         }
       }
-    console.log('thisBooking.booked',thisBooking.booked );
+      console.log('thisBooking.booked',thisBooking.booked );
     }
   }
   makeBooked(date, hour, duration,table){
@@ -131,7 +133,7 @@ class Booking{
         thisBooking.booked[date][hourBlock] = [];
       }
 
-    thisBooking.booked[date][hourBlock].push(table);// dodanie zarezerwowanego stolika do tablicy z rezerwacjami
+      thisBooking.booked[date][hourBlock].push(table);// dodanie zarezerwowanego stolika do tablicy z rezerwacjami
     }
   }
 }
